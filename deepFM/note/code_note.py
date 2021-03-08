36b3f34a88434971fbac9f79e4152dc0a2263334
lijiankou <lijiankou@bigo.sg>
2021-03-08 14:30:52 +0800

    update

--- deepFM/note/code_note.py
+++ deepFM/note/code_note.py
@@ -1,9 +1,10 @@
 def __init__(self, user_count, item_count, cate_count, cate_list):
   #u  user_id
-  #i item_id
-  #j cate_id
+  #i 正样本 item_id
+  #j 负样本 item_id
   #hist_i history item list
   #y label (0,1)
+  #cate_list item到cate的映射关系, tf 
   self.u = tf.placeholder(tf.int32, [None,]) # [B]
   self.i = tf.placeholder(tf.int32, [None,]) # [B]
   self.j = tf.placeholder(tf.int32, [None,]) # [B]
